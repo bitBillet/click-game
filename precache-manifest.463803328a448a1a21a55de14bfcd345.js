287ec76249d40787c400ddaf7c6303743fdb4be9
sasha <dogidog2011@mail.ru>
2020-06-17 07:02:06 +0300

    Updates

---
+++ precache-manifest.463803328a448a1a21a55de14bfcd345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15e1e7c8c933213d9c4d1a0676d5b30",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "6cf9f76df9af5bc7b7d2",
+    "url": "/click-game/static/css/main.6fcccd32.chunk.css"
+  },
+  {
+    "revision": "018bd96391c712f1cbb1",
+    "url": "/click-game/static/js/2.81bc2e84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/click-game/static/js/2.81bc2e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf9f76df9af5bc7b7d2",
+    "url": "/click-game/static/js/main.22467cf9.chunk.js"
+  },
+  {
+    "revision": "5699508e9d88e3ebb54f",
+    "url": "/click-game/static/js/runtime-main.ec4d9082.js"
+  }
+]);
